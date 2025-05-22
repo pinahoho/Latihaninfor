40b1508a496e3ee58433a75cc90955f7859ed0e2
pinahoho <fadhilapina@gmail.com>
2025-05-22 21:44:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title("Hello selamat datang di web serunya belajar ngoding")
 st.write(
     "belajar ngoding seru bersama pina"
+) 
+st. image("anak-gajah1.jpg", width=200) 
